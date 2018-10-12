f26c35ae6ff0eb48dbdf0e27fdf076df8fc20000
wxj <18829342274@163.com>
2018-10-12 17:39:40 +0800

    init

---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,30 @@
+
+<nav class="navbar navbar-default navbar-static-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                <span class="sr-only">Toggle Navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a href="{{url('/')}}" class="navbar-brand">
+                LaraBBS
+            </a>
+        </div>
+
+        <div class="collapse navbar-collapse" id="app-navbar-collapse">
+            <!-- left side of navbar-->
+            <ul class="nav navbar-nav">
+
+            </ul>
+
+            <!-- right side of navbar -->
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#">登录</a></li>
+                <li><a href="#">注册</a></li>
+            </ul>
+
+        </div>
+    </div>
+</nav>
