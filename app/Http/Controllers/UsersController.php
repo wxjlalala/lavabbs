849728b8f2c1819fdb3335f0b409ef816fc2a7dc
wxj <18829342274@163.com>
2018-10-15 17:31:00 +0800

    上传头像

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Handlers\ImageUploadHandler;
 use App\Http\Requests\UserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -10,18 +11,25 @@ class UsersController extends Controller
 {
     public function show(User $user)
     {
-        return view('users.show',compact('user'));
+        return view('users.show', compact('user'));
     }
 
     public function edit(User $user)
     {
-        return view('users.edit',compact('user'));
+        return view('users.edit', compact('user'));
     }
 
-    public function update(UserRequest $userRequest, User $user)
+    public function update(UserRequest $userRequest, User $user, ImageUploadHandler $uploader)
     {
-        $user->update($userRequest->all());
-        return redirect()->route('users.show',$user->id)->with('success','个人资料更新成功');
+        $data = $userRequest->all();
+        if ($userRequest->has('avatar')) {
+            $result = $uploader->save($data['avatar'], 'avatar', $user->id,360);
+            if($result){
+                $data['avatar'] = $result['path'];
+            }
+        }
+        $user->update($data);
+        return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功');
     }
 
 }
