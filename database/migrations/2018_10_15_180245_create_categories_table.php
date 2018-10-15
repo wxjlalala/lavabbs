b4aeaa02d4b4073b6812bc0775ccf1c97d83ffc0
wxj <18829342274@163.com>
2018-10-15 18:12:31 +0800

    添加分类

---
+++ database/migrations/2018_10_15_180245_create_categories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->index()->commont('名称');
+            $table->text('description')->nullable()->commont('描述');
+            $table->integer('post_count')->default(0)->commont('帖子数');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
