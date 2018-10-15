98f5cdba6c1de5fa15c180b4c45a9436d3f55af5
wxj <18829342274@163.com>
2018-10-15 15:18:08 +0800

    编辑修改个人信息

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -12,14 +13,15 @@ class UsersController extends Controller
         return view('users.show',compact('user'));
     }
 
-    public function edit()
+    public function edit(User $user)
     {
-
+        return view('users.edit',compact('user'));
     }
 
-    public function update()
+    public function update(UserRequest $userRequest, User $user)
     {
-
+        $user->update($userRequest->all());
+        return redirect()->route('users.show',$user->id)->with('success','个人资料更新成功');
     }
 
 }
