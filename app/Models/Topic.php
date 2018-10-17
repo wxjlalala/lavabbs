425b01924c17867187995eb449ba8a15391b0392
wxj <18829342274@163.com>
2018-10-17 17:06:24 +0800

    新建话题

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 class Topic extends Model
 {
-    protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+    protected $fillable = ['title', 'body', 'category_id', 'excerpt', 'slug'];
 
     public function category()
     {
@@ -16,10 +16,9 @@ class Topic extends Model
         return $this->belongsTo(User::class);
     }
 
-    public function scopeWithOrder($query,$order)
+    public function scopeWithOrder($query, $order)
     {
-        switch ($order)
-        {
+        switch ($order) {
             case 'recent':
                 $query->recent();
                 break;
@@ -27,16 +26,16 @@ class Topic extends Model
                 $query->recentReplied();
                 break;
         }
-        $query->with('user','category');
+        $query->with('user', 'category');
     }
 
     public function scopeRecent($query)
     {
-        return $query->orderBy('created_at','desc');
+        return $query->orderBy('created_at', 'desc');
     }
 
     public function scopeRecentReplied($query)
     {
-        return $query->orderBy('updated_at','desc');
+        return $query->orderBy('updated_at', 'desc');
     }
 }
--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -9,3 +9,9 @@ function get_category()
 {
     return \App\Models\Category::all()->toArray();
 }
+
+function make_excerpt($value, $length = 200)
+{
+    $excerpt = trim(preg_replace('/\r\n|\r|\n+/', ' ', strip_tags($value)));
+    return str_limit($excerpt, $length);
+}
