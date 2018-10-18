9ced7cba27833b86fadfdfa12acd77ab1f4fa519
wxj <18829342274@163.com>
2018-10-18 17:02:10 +0800

    编辑帖子，添加授权

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -48,7 +48,7 @@
                         <a href="{{ route('topics.edit', $topic->id) }}" class="btn btn-default btn-xs" role="button">
                             <i class="glyphicon glyphicon-edit"></i> 编辑
                         </a>
-                        <a href="#" class="btn btn-default btn-xs" role="button">
+                        <a href="{{ route('topics.destroy',$topic->id) }}" class="btn btn-default btn-xs" role="button">
                             <i class="glyphicon glyphicon-trash"></i> 删除
                         </a>
                     </div>
