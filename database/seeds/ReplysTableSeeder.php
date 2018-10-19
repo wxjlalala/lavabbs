5190d32a96905727d9ff3adb04b6b908263947d5
wxj <18829342274@163.com>
2018-10-19 16:27:20 +0800

    回复列表

---
+++ database/seeds/ReplysTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Reply;
+
+class ReplysTableSeeder extends Seeder
+{
+    public function run()
+    {
+        // 所有用户id
+        $user_ids = \App\Models\User::all()->pluck('id')->toArray();
+
+        // 所有话题id
+        $topic_ids = \App\Models\Topic::all()->pluck('id')->toArray();
+
+        $faker = app(\Faker\Generator::class);
+
+        $replys = factory(Reply::class)->times(1000)->make()->each(function ($reply, $index) use($user_ids,$topic_ids,$faker){
+            $reply->user_id = $faker->randomElement($user_ids);
+            $reply->topic_id = $faker->randomElement($topic_ids);
+        });
+
+        Reply::insert($replys->toArray());
+    }
+
+}
+
