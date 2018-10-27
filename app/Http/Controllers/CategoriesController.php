1cd00ec443da055652a0078b23017935025625b0
wxj <18829342274@163.com>
2018-10-27 16:52:15 +0800

    侧边活跃用户

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -4,14 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use App\Models\Topic;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class CategoriesController extends Controller
 {
 
-    public function show(Category $category,Request $request, Topic $topic)
+    public function show(Category $category, Request $request, Topic $topic, User $user)
     {
-        $topics = $topic->withOrder($request->order)->where('category_id',$category->id)->paginate(20);
-        return view('topics.index',compact('topics','category'));
+        $topics = $topic->withOrder($request->order)->where('category_id', $category->id)->paginate(20);
+        // 活跃用户列表
+        $active_users = $user->getActiveUsers();
+        return view('topics.index', compact('topics', 'category', 'active_users'));
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -13,6 +13,8 @@ class User extends Authenticatable
     use Notifiable {
         notify as protected laravelNotify;
     }
+    use Traits\ActiveUserHelper;
+
     public function notify($instance)
     {
         // 如果要通知的人是当前用户，就不必通知了！
