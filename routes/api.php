9b362da219d147b8df8998eeb534b9e7ddd208e7
wxj <18829342274@163.com>
2018-11-02 16:11:30 +0800

    分类，添加修改话题

--- routes/api.php
+++ routes/api.php
@@ -20,14 +20,14 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 $api = app('Dingo\Api\Routing\Router');
 $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api',
-    'middleware' => 'serializer:array',
-], function($api) {
+    'middleware' => ['serializer:array', 'bindings'],
+], function ($api) {
 
     $api->group([
         'middleware' => 'api.throttle',
         'limit' => config('api.rate_limits.sign.limit'),
         'expires' => config('api.rate_limits.sign.expires'),
-    ],function ($api){
+    ], function ($api) {
         // 游客可以访问的接口
 
         // 短信验证码
@@ -37,7 +37,7 @@ $api->version('v1', [
         // 图片验证码
         $api->post('captchas', 'CaptchasController@store')->name('api.captchas.store');
         // 第三方登录
-        $api->post('socials/{social_type}/authorizations','AuthorizationsController@socialStore')->name('api.socials.authorizations.store');
+        $api->post('socials/{social_type}/authorizations', 'AuthorizationsController@socialStore')->name('api.socials.authorizations.store');
         // 登录
         $api->post('authorizations', 'AuthorizationsController@store')->name('api.authorizations.store');
         // 刷新token
@@ -51,13 +51,17 @@ $api->version('v1', [
 
         // 需要 token 验证的接口
 
-        $api->group(['middleware' => 'api.auth'], function($api) {
+        $api->group(['middleware' => 'api.auth'], function ($api) {
             // 当前登录用户信息
             $api->get('user', 'UsersController@me')->name('api.user.show');
             // 编辑登录用户信息
             $api->patch('user', 'UsersController@update')->name('api.user.update');
             // 图片资源
-            $api->post('images','ImagesController@store')->name('api.images.store');
+            $api->post('images', 'ImagesController@store')->name('api.images.store');
+            // 发布话题
+            $api->post('topics', 'TopicsController@store')->name('api.topics.store');
+            // 修改话题
+            $api->patch('topics/{topic}', 'TopicsController@update')->name('api.topics.update');
 
         });
 
